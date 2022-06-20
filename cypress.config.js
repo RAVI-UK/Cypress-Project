ca88018211c08b1f214072a3eb707d03e5d1a303
RAVI-UK <107795904+RAVI-UK@users.noreply.github.com>
2022-06-20 08:37:02 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,9 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   projectId: '2keb6e',
+  env: {
+    CYPRESS_RECORD_KEY: 'b8339e48-8683-42dd-8303-a64ef256d6bd'
+  },
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
